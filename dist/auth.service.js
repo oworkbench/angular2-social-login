034b7fddd1dca1c2708019d6da60d0032cab40ff
oworkbench <oleg.workbench@gmail.com>
2017-07-06 23:36:33 +0300

    Update auth.service.js

--- dist/auth.service.js
+++ dist/auth.service.js
@@ -33,13 +33,15 @@ var AuthService = (function () {
                 case "facebook":
                     FB.getLoginStatus(function (response) {
                         if (response.status === "connected") {
-                            FB.api('/me?fields=name,email,picture', function (res) {
+                            FB.api('/me?fields=name,first_name,last_name,email,picture', function (res) {
                                 if (!res || res.error) {
                                     observer.error(res.error);
                                 }
                                 else {
                                     var userDetails = {
                                         name: res.name,
+                                        firstName: res.first_name,
+                                        lastName: res.last_name,
                                         email: res.email,
                                         uid: res.id,
                                         provider: "facebook",
